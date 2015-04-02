972bee2c91745e2cfb321bcb06f9a28762c37785
sigridsimmel <sigrid.simmel@gmail.com>
2015-04-02 14:21:27 +0200

    Create store-products.js

---
+++ app/products/store-products.js
@@ -0,0 +1,32 @@
+[
+	{	
+		"id": 1,
+		"name": "Bamse",
+		"description": "Stor plysbamse.",
+		"price": 175,
+		"imageThumbnail": "assets/images/teddy.jpg",
+		"image": "assets/images/teddy_big.jpg",
+		"descriptionLong": "Dejlig blød bamse, som med garanti vil stjæle barnets hjerte fra starten. Super god barselsgave til både drenge og piger med potentiale til at blive yndlingsbamsen.",
+		"size": "39"
+	},
+	{
+		"id": 2,
+		"name": "Gyngehest",
+		"description": "Flot gyngehest for de små.",
+		"price": 350,
+		"imageThumbnail": "assets/images/rocking-horse.jpg",
+		"image": "assets/images/rocking-horse_big.jpg",
+		"descriptionLong": "Klassisk gyngehest i massiv bøg med bomuld i manke og hale. Hjælper barnet med at træne motorikken på en sjov måde. Særligt egnet til børn i alderen 1-2 år.",
+		"size": "67"
+	},
+	{
+		"id": 3,
+		"name": "Puslespil",
+		"description": "Puslespil for de mindste.",
+		"price": 125,
+		"imageThumbnail": "assets/images/puzzle.jpg",
+		"image": "assets/images/puzzle_big.jpg",
+		"descriptionLong": "Dette fine klodspuslespil består af af 9 træklodser. Vend dem rundt og få flotte billeder frem. 6 forskellige motiver.",
+		"size": "20"
+	}
+]
